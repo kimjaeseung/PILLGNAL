49ca899fecf9418e6d4b17e98d49c1ee34b371fa
엄재웅 <zzang6535@gmail.com>
2021-03-23 16:26:57 +0900

    :bulb: [ADD] create backend project #S04P23A403-82

---
+++ backend/src/main/java/com/pillgnal/backend/config/Swagger2Config.java
@@ -0,0 +1,35 @@
+package com.pillgnal.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class Swagger2Config {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.pillgnal.backend"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Pillgnal API Document")
+                .version("v0.1")
+                .description("Pillgnal 서비스에 사용될 API 문서 예시입니다")
+                .license("SSAFY A403")
+                .build();
+    }
+}
+
