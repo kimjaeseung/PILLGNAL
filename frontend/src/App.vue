f87ecc770a27cd22dd9517769f60aae33bdce097
윤혜민 <h2m0@swu.ac.kr>
2021-03-23 16:47:19 +0900

    ⛏ [EDIT] make setting frame #S04P23A403-75

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,8 @@
 <template>
   <v-app>
-    app.vue
-    <router-link to="/prescription">aa</router-link>
     <router-view></router-view>
   </v-app>
 </template>
-
 <script>
 export default {
   name: 'App',
