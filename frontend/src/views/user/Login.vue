d27a34483e21ac78b12cbfe8900daad9206078e6
wotjd1163@git config --global user.email kimjea23@naver.com <kimjea23@naver.com>
2021-03-24 11:35:00 +0900

    :hammer: [EDIT] update regist page valid #S04P23A403-89

--- frontend/src/views/user/Login.vue
+++ frontend/src/views/user/Login.vue
@@ -1,56 +1,79 @@
 <template>
   <v-app>
     <v-layout fill-height align-center>
-      <v-container style="max-width: 500px">
-        <v-row>
-          <v-col class="text-center text-h4"> 로그인 </v-col>
+      <v-container>
+        <v-row class="mb-3">
+          <v-col class="text-center"> <h1>로그인</h1> </v-col>
         </v-row>
         <v-row>
-          <v-col>
+          <v-col class="pa-0">
             <v-text-field
-              v-model="description"
-              :rules="rules"
-              counter
-              maxlength="25"
-              hint="This field uses maxlength attribute"
-              label="Email"
+              v-model="email"
+              :rules="[emailRules.required, emailRules.email]"
+              hint="Invalid e-mail."
+              label="E-mail"
               outlined
+              flat
+              solo-inverted
             ></v-text-field>
           </v-col>
         </v-row>
         <v-row>
-          <v-col>
+          <v-col class="pa-0">
             <v-text-field
-              v-model="description"
-              :rules="rules"
-              counter
-              maxlength="25"
-              hint="This field uses maxlength attribute"
+              v-model="password"
+              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[passwordRules.required, passwordRules.min]"
+              :type="show ? 'text' : 'password'"
               label="Password"
+              hint="At least 8 characters"
+              class="input-group--focused"
+              @click:append="show = !show"
               outlined
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col>
-            <v-text-field
-              v-model="description"
-              :rules="rules"
-              counter
-              maxlength="25"
-              hint="This field uses maxlength attribute"
-              label="Password"
-              outlined
+              count
+              flat
+              solo-inverted
             ></v-text-field>
           </v-col>
         </v-row>
       </v-container>
     </v-layout>
+    <v-btn x-large tile color="primary">로그인</v-btn>
   </v-app>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      name: '',
+      phoneNumber: '',
+      show: false,
+      emailRules: {
+        required: (value) => !!value || 'Required.',
+        counter: (value) => value.length <= 20 || 'Max 20 characters',
+        email: (value) => {
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || 'Invalid e-mail.';
+        },
+      },
+      passwordRules: {
+        required: (value) => !!value || 'Required.',
+        min: (v) => v.length >= 8 || 'Min 8 characters',
+      },
+    };
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.v-card {
+  overflow: auto;
+}
+.container {
+  max-width: 500px;
+  width: 80%;
+}
+</style>
--- frontend/src/views/user/Regist.vue
+++ frontend/src/views/user/Regist.vue
@@ -5,65 +5,70 @@
         <v-row class="mb-3">
           <v-col class="text-center"> <h1>회원가입</h1> </v-col>
         </v-row>
-        <v-row>
-          <v-col class="pa-0">
-            <v-text-field
-              v-model="email"
-              :rules="[emailRules.required, emailRules.email]"
-              hint="Invalid e-mail."
-              label="E-mail"
-              outlined
-              flat
-              solo-inverted
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col class="pa-0">
-            <v-text-field
-              v-model="password"
-              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-              :rules="[passwordRules.required, passwordRules.min]"
-              :type="show ? 'text' : 'password'"
-              label="Password"
-              hint="At least 8 characters"
-              class="input-group--focused"
-              @click:append="show = !show"
-              outlined
-              count
-              flat
-              solo-inverted
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col class="pa-0">
-            <v-text-field
-              v-model="name"
-              :rules="[nameRules.required, nameRules.min]"
-              counter
-              maxlength="25"
-              hint="This field uses maxlength attribute"
-              label="User name"
-              outlined
-              flat
-              solo-inverted
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-col class="pa-0">
-            <v-text-field
-              v-model="phoneNumber"
-              :rules="[phoneNumberRules.required, phoneNumberRules.phoneNumber]"
-              hint="Please include hyphens"
-              label="Phone number"
-              outlined
-              flat
-              solo-inverted
-            ></v-text-field>
-          </v-col>
-        </v-row>
+        <v-form v-model="valid">
+          <v-row>
+            <v-col class="pa-0">
+              <v-text-field
+                v-model="email"
+                :rules="[emailRules.required, emailRules.email]"
+                hint="Invalid e-mail."
+                label="E-mail"
+                outlined
+                flat
+                solo-inverted
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="pa-0">
+              <v-text-field
+                v-model="password"
+                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+                :rules="[passwordRules.required, passwordRules.min]"
+                :type="show ? 'text' : 'password'"
+                maxlength="25"
+                label="Password"
+                hint="At least 8 characters"
+                class="input-group--focused"
+                @click:append="show = !show"
+                outlined
+                flat
+                solo-inverted
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="pa-0">
+              <v-text-field
+                v-model="name"
+                :rules="[nameRules.required, nameRules.min]"
+                counter
+                maxlength="25"
+                hint="At least 2 characters"
+                label="User name"
+                outlined
+                flat
+                solo-inverted
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col class="pa-0">
+              <v-text-field
+                v-model="phoneNumber"
+                :rules="[
+                  phoneNumberRules.required,
+                  phoneNumberRules.phoneNumber,
+                ]"
+                hint="Please include hyphens"
+                label="Phone number"
+                outlined
+                flat
+                solo-inverted
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-form>
         <v-row>
           <v-col class="px-0">
             <v-card height="200px">
@@ -74,12 +79,14 @@
         </v-row>
         <v-row>
           <v-col cols="6" class="pa-0"
-            ><v-checkbox label="동의합니다."></v-checkbox
+            ><v-checkbox v-model="termsCheck" label="동의합니다."></v-checkbox
           ></v-col>
         </v-row>
       </v-container>
     </v-layout>
-    <v-btn x-large tile color="primary">가입하기</v-btn>
+    <v-btn x-large tile color="primary" :disabled="!valid || !termsCheck"
+      >가입하기</v-btn
+    >
   </v-app>
 </template>
 
@@ -91,7 +98,9 @@ export default {
       password: '',
       name: '',
       phoneNumber: '',
+      termsCheck: false,
       show: false,
+      valid: false,
       emailRules: {
         required: (value) => !!value || 'Required.',
         counter: (value) => value.length <= 20 || 'Max 20 characters',
@@ -111,8 +120,7 @@ export default {
       phoneNumberRules: {
         required: (value) => !!value || 'Required.',
         phoneNumber: (value) => {
-          const pattern = /^[0-9-]*$/;
-          console.log(pattern.test(value));
+          const pattern = /^\d{3}-\d{3,4}-\d{4}$/;
           return pattern.test(value) || 'Invalid phone number.';
         },
       },
