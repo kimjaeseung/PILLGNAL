e8b83973a9cf94bc074e4a1a021f419dde1681ff
윤혜민 <h2m0@swu.ac.kr>
2021-03-24 15:32:44 +0900

    :hammer: [EDIT] create family link pages #S04P23A403-8 #S04P23A403-78

--- frontend/src/views/settings/Settings.vue
+++ frontend/src/views/settings/Settings.vue
@@ -5,21 +5,21 @@
       <v-row class="mt-12">
         <v-col>
           <v-card @click="router(0)" outlined class="text-center mb-10">
-            <v-avatar class="mt-7" tile><img src="@/assets/link.svg" alt="John"/></v-avatar>
+            <v-avatar class="mt-7" tile><img src="@/assets/settings/link.svg" /></v-avatar>
             <v-card-subtitle>가족 연결 관리하기</v-card-subtitle></v-card
           >
           <v-card @click="router(1)" outlined class="text-center">
-            <v-avatar class="mt-7"><img src="@/assets/clock.svg" alt="John"/></v-avatar>
-            <v-card-subtitle> 기본 시간 설정하기</v-card-subtitle></v-card
+            <v-avatar class="mt-7" tile><img src="@/assets/settings/clock.svg" /></v-avatar>
+            <v-card-subtitle>기본 시간 설정하기</v-card-subtitle></v-card
           >
         </v-col>
         <v-col>
           <v-card @click="router(2)" outlined class="text-center mb-10">
-            <v-avatar class="mt-7"><img src="@/assets/help.svg" alt="John"/></v-avatar>
-            <v-card-subtitle> Pillgnal 도움말 보기</v-card-subtitle></v-card
+            <v-avatar class="mt-7" tile><img src="@/assets/settings/help.svg" /></v-avatar>
+            <v-card-subtitle>Pillgnal 도움말 보기</v-card-subtitle></v-card
           >
           <v-card @click="router(3)" outlined class="text-center">
-            <v-avatar class="mt-7"><img src="@/assets/attention.svg" alt="John"/></v-avatar>
+            <v-avatar class="mt-7" tile><img src="@/assets/settings/attention.svg" /></v-avatar>
             <v-card-subtitle>주의 성분 관리</v-card-subtitle></v-card
           >
         </v-col>
@@ -33,9 +33,7 @@
       <v-row>
         <v-col>
           <v-card outlined class="text-center">
-            <v-card-actions id="logout" class="text-center card-center">
-              로그아웃
-            </v-card-actions>
+            <v-card-actions id="logout" class="text-center card-center"> 로그아웃 </v-card-actions>
           </v-card>
         </v-col>
       </v-row>
@@ -44,15 +42,15 @@
 </template>
 <script>
 export default {
-data : () => ({
-path: ['family', 'ff', 'tutorial', 'attention', 'service']
-}),
-methods: {
-    router: function(n) {
-        this.$router.push(this.path[n]);
-    }
-},
-}
+  data: () => ({
+    path: ['familylist', 'none', 'tutorial', 'attention', 'service'],
+  }),
+  methods: {
+    router: function (n) {
+      this.$router.push(this.path[n]);
+    },
+  },
+};
 </script>
 <style scoped>
 #logout {
